b12b11e01fc8c9936d311945fd1f27b248ea9850
Gabriel Colombo <gcolombo07@gmail.com>
2022-03-18 20:38:12 -0300

    Implementing the getTaskById method in both service and controller

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -11,6 +11,10 @@ export class TasksService {
     return this.tasks;
   }
 
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
   createTask(createTaskDto: CreateTaskDTO): Task {
     const { title, description } = createTaskDto;
 
