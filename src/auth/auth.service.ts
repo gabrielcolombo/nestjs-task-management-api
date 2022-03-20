2ea33c69701b4cbf8008f030ef0a0ba640cfffc4
Gabriel Colombo <gcolombo07@gmail.com>
2022-03-20 19:48:37 -0300

    Implementing a JwtPayload interface and signing the jwt token on authentication

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,25 +3,33 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDTO } from './dto/auth-credentials.dto';
 import { UsersRepository } from './users.repository';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UsersRepository)
     private usersRepository: UsersRepository,
+    private jwtService: JwtService,
   ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDTO): Promise<void> {
     return this.usersRepository.createUser(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDTO): Promise<string> {
+  async signIn(
+    authCredentialsDto: AuthCredentialsDTO,
+  ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialsDto;
 
     const user = await this.usersRepository.findOne({ username });
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      return 'success';
+      const payload: JwtPayload = { username };
+      const accessToken: string = await this.jwtService.sign(payload);
+
+      return { accessToken };
     }
 
     throw new UnauthorizedException('Please check your login credentials');
