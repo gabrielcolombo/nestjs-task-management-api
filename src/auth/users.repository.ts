d639c647b90719f8042754ad9be6f0569cffd82d
Gabriel Colombo <gcolombo07@gmail.com>
2022-03-20 18:56:18 -0300

    Fixing user entity and implementing a signUp method

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,5 +1,14 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDTO } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
 @EntityRepository(User)
-export class UsersRepository extends Repository<User> {}
+export class UsersRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDTO): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = this.create({ username, password });
+
+    await this.save(user);
+  }
+}
